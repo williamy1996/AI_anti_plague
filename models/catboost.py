5e2be4a400bec34e755ccee32c3c1d9438ecafa2
williamy1996 <34211130+williamy1996@users.noreply.github.com>
2020-02-21 22:10:41 +0800

    Update catboost.py

--- models/catboost.py
+++ models/catboost.py
@@ -5,18 +5,15 @@ from utils.smape import smape
 
 class catboostRegressor():
     def __init__(self, n_estimators, learning_rate, num_leaves, min_child_samples,
-                 subsample, colsample_bylevel, reg_alpha, reg_lambda, loss_function):
+                 subsample, colsample_bylevel, reg_lambda, loss_function):
         self.n_estimators = n_estimators
         self.learning_rate = learning_rate
         self.num_leaves = num_leaves
         self.subsample = subsample
-        self.reg_alpha = reg_alpha
         self.reg_lambda = reg_lambda
         self.min_child_samples = min_child_samples
         self.colsample_bylevel = colsample_bylevel
         self.loss_function = loss_function
-
-        self.n_jobs = -1
         self.estimator = None
 
     def fit(self, X, y, metric=smape):
@@ -27,9 +24,7 @@ class catboostRegressor():
                                        min_child_samples=self.min_child_samples,
                                        subsample=self.subsample,
                                        colsample_bylevel=self.colsample_bylevel,
-                                       reg_alpha=self.reg_alpha,
                                        reg_lambda=self.reg_lambda,
-                                       n_jobs=self.n_jobs
                                        loss_function=self.loss_function)
         self.estimator.fit(X, y, eval_metric=metric)
         return self
